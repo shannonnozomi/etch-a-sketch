e033c2fb42f51327228fbe49ae30edcfcd20e239
Shannon Nicholas <shannonnozomi@gmail.com>
2021-11-08 13:29:56 -0500

    cleaned up index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var pixels = document.getElementsByClassName("pixels");
-var input = document.getElementById("colorPicker");
+const pixels = document.getElementsByClassName("pixels");
+const input = document.getElementById("colorPicker");
 
-function pixelGenerator() {
-  var div = document.createElement("div");
-  div.style.width = "30px";
-  div.style.height = "30px";
+function bigPixelGenerator() {
+  const div = document.createElement("div");
+  div.style.width = "25px";
+  div.style.height = "25px";
+  div.textContent = "";
+  div.style.border = "solid .5px white";
+  div.className = "pixels";
+  document.getElementById("main").appendChild(div);
+  div.addEventListener("mouseenter", function (e) {
+    e.target.style.backgroundColor = input.value;
+  });
+}
+function smallPixelGenerator() {
+  const div = document.createElement("div");
+  div.style.width = "10px";
+  div.style.height = "10px";
   div.textContent = "";
   div.style.border = "solid .5px white";
   div.className = "pixels";
@@ -14,26 +26,65 @@ function pixelGenerator() {
   });
 }
 
-for (var i = 0; i < 520; i++) {
-  pixelGenerator();
-  if (pixels.length === 520) {
-    pixels[0].style.borderRadius = "30px 0 0 0";
-    pixels[25].style.borderRadius = "0 30px 0 0";
-    pixels[494].style.borderRadius = "0 0 0 30px";
-    pixels[519].style.borderRadius = "0 0 30px 0";
+function bigPixelCorners() {
+  if (pixels.length === 768) {
+    pixels[0].style.borderRadius = "10px 0 0 0";
+    pixels[31].style.borderRadius = "0 10px 0 0";
+    pixels[736].style.borderRadius = "0 0 0 10px";
+    pixels[767].style.borderRadius = "0 0 10px 0";
   }
-  console.log(input);
+}
+function smallPixelCorners() {
+  if (pixels.length === 4800) {
+    pixels[0].style.borderRadius = "30px 0 0 0";
+    pixels[79].style.borderRadius = "0 30px 0 0";
+    pixels[4720].style.borderRadius = "0 0 0 30px";
+    pixels[4799].style.borderRadius = "0 0 30px 0";
+  }
 }
 
+for (var i = 0; i < 768; i++) {
+  bigPixelGenerator();
+  bigPixelCorners();
+}
+
+function sizeAdjust() {
+  const big = document.getElementById("big");
+  const small = document.getElementById("small");
+  if (big.innerHTML === "BIG") {
+    big.style.cssText = null;
+    small.style.cssText = "background-color: gold; font-size: 35px;";
+    big.innerHTML = "big";
+    small.innerHTML = "SMAll";
+    document.getElementById("main").innerHTML = "";
+    for (var i = 0; i < 4800; i++) {
+      smallPixelGenerator();
+      smallPixelCorners();
+    }
+  } else {
+    small.style.cssText = null;
+    big.style.cssText = "background-color: gold; font-size: 35px;";
+    big.innerHTML = "BIG";
+    small.innerHTML = "small";
+    document.getElementById("main").innerHTML = "";
+    for (var i = 0; i < 768; i++) {
+      bigPixelGenerator();
+      bigPixelCorners();
+    }
+  }
+}
 function refresh() {
-  document.getElementById("main").innerHTML = "";
-  for (var i = 0; i < 520; i++) {
-    pixelGenerator();
-    if (pixels.length === 520) {
-      pixels[0].style.borderRadius = "30px 0 0 0";
-      pixels[25].style.borderRadius = "0 30px 0 0";
-      pixels[494].style.borderRadius = "0 0 0 30px";
-      pixels[519].style.borderRadius = "0 0 30px 0";
+  if (pixels.length < 800) {
+    document.getElementById("main").innerHTML = "";
+    for (var i = 0; i < 768; i++) {
+      bigPixelGenerator();
+      bigPixelCorners();
+    }
+  } else if (pixels.length > 760) {
+    document.getElementById("main").innerHTML = "";
+    for (var i = 0; i < 4800; i++) {
+      smallPixelGenerator();
+      smallPixelCorners();
     }
   }
 }
